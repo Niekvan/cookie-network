76525fcee0fbb8f7336464f06f570ae3f20cacd2
Niek <17721120+Niekvan@users.noreply.github.com>
2019-06-13 01:07:20 +0200

    update icon

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,13 +11,13 @@ export default {
    ** Headers of the page
    */
   head: {
-    title: pkg.name,
+    title: 'The Cookie Network',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: pkg.description }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    link: [{ rel: 'icon', type: 'image/png', href: '/icon.png' }]
   },
 
   /*
